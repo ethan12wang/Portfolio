3b4d5425d8a37a2655aae211f0ce3c0a41f2f850
ethan12wang <151788256+ethan12wang@users.noreply.github.com>
2024-08-04 15:46:05 -0700

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const carousel = document.querySelector('.carousel-images');
+  const images = document.querySelectorAll('.carousel-images img');
+  const prevButton = document.querySelector('.carousel-button.prev');
+  const nextButton = document.querySelector('.carousel-button.next');
+  
+  let index = 0;
 
+  function updateCarousel() {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  prevButton.addEventListener('click', () => {
+    index = (index > 0) ? index - 1 : images.length - 1;
+    updateCarousel();
+  });
+
+  nextButton.addEventListener('click', () => {
+    index = (index < images.length - 1) ? index + 1 : 0;
+    updateCarousel();
+  });
+});
